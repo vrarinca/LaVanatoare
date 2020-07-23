a7241f3dd1fc66046db2693cab3784910a2c88bc
David Voicu <vdavidxx@yahoo.com>
2020-07-23 17:50:37 +0300

    try2

--- UIDesign/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ UIDesign/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HuntingAssociation.Models;
+
 using HuntingAssociation.Views.DTOs;
 
 namespace HuntingAssociation.Controllers
